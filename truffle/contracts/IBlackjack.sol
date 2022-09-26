4b36ed40b79f44a612073b261d51ca302a1be418
everGreenGH <skin0011@naver.com>
2022-09-26 18:55:20 +0900

    chore: prettier 세팅

--- truffle/contracts/IBlackjack.sol
+++ truffle/contracts/IBlackjack.sol
@@ -1,64 +1,78 @@
 pragma solidity ^0.8.15;
 
 interface IBlackjack {
+    enum Stage {
+        Bet,
+        PlayHand,
+        ConcludeHands
+    }
 
-  enum Stage {
-    Bet,
-    PlayHand,
-    ConcludeHands
-  }
+    struct Game {
+        uint256 id;
+        uint64 startTime;
+        uint64 round;
+        Stage stage;
+        Player dealer;
+        Player player;
+    }
 
-  struct Game {
-    uint id;
-    uint64 startTime;
-    uint64 round;
-    Stage stage;
-    Player dealer;
-    Player player;
-  }
+    struct Player {
+        uint256 bet;
+        uint8 score;
+        uint256[] hand;
+    }
 
-  struct Player {
-    uint bet;
-    uint8 score;
-    uint[] hand;
-  }
+    event StageChanged(uint256 gameId, uint64 round, Stage newStage);
+    event NewRound(uint256 gameId, uint64 round, address player, uint256 bet);
+    event CardDrawn(uint256 gameId, uint64 round, uint8 cardValue, uint8 score);
+    event Result(uint256 gameId, uint64 round, uint256 payout, uint8 playerScore, uint8 dealerScore);
+    event PlayerHand(uint256 gameId, uint256[] playerHand);
+    event Received(address, uint256);
 
-  event StageChanged(uint gameId, uint64 round, Stage newStage);
-  event NewRound(uint gameId, uint64 round, address player, uint bet);
-  event CardDrawn(uint gameId, uint64 round, uint8 cardValue, uint8 score);
-  event Result(uint gameId, uint64 round, uint payout, uint8 playerScore, uint8 dealerScore);
-  event PlayerHand(uint gameId, uint[] playerHand);
-  event Received(address, uint);
+    function kill() external;
 
-  function kill() external;
+    /// @dev Start a new round of Blackjack
+    function newRound() external payable;
 
-  /// @dev Start a new round of Blackjack
-  function newRound() external payable;
+    /// @dev Take one additional card
+    function hit() external;
 
-  /// @dev Take one additional card
-  function hit() external;
+    /// @dev Taking no more cards and ended the game
+    function stand() external;
 
-  /// @dev Taking no more cards and ended the game
-  function stand() external;
+    /// Getters
+    /// @dev Get dealer state
+    /// @return hand The dealer's hand
+    /// @return score The dealer's score
+    function getDealerState() external view returns (uint256[] memory hand, uint256 score);
 
-  /// Getters
-  /// @dev Get dealer state
-  /// @return hand The dealer's hand
-  /// @return score The dealer's score
-  function getDealerState() external view returns (uint[] memory hand, uint score);
+    /// @dev Get player state
+    /// @return hand The player's hand
+    /// @return score The player's score
+    /// @return bet Original bet at start of hand
+    function getPlayerState()
+        external
+        view
+        returns (
+            uint256[] memory hand,
+            uint256 score,
+            uint256 bet
+        );
 
-  /// @dev Get player state
-  /// @return hand The player's hand
-  /// @return score The player's score
-  /// @return bet Original bet at start of hand
-  function getPlayerState() external view returns (uint[] memory hand, uint score, uint bet);
-
-  /// @dev Get game stage
-  /// @return gameId ID for the current Blackjack game
-  /// @return startTime Time the current Blackjack game began
-  /// @return gameMaxBet Max bet allowed to be placed for new game
-  /// @return round Number of round of Blackjack game played
-  /// @return stage Stage of the Blackjack game
-  function getGameState() external view returns (uint gameId, uint64 startTime, uint gameMaxBet, uint64 round, Stage stage);
-  
+    /// @dev Get game stage
+    /// @return gameId ID for the current Blackjack game
+    /// @return startTime Time the current Blackjack game began
+    /// @return gameMaxBet Max bet allowed to be placed for new game
+    /// @return round Number of round of Blackjack game played
+    /// @return stage Stage of the Blackjack game
+    function getGameState()
+        external
+        view
+        returns (
+            uint256 gameId,
+            uint64 startTime,
+            uint256 gameMaxBet,
+            uint64 round,
+            Stage stage
+        );
 }
